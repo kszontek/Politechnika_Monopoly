44fdb8061bbd98d24c35f7bb6e821ced78e6b430
Piotr Czyzewski <pzya66939@gmail.com>
2026-06-10 19:20:10 +0200

    Panel gracza oraz profil ze statatmi zarys

--- src/main/java/pl/pb/tc/config/SecurityConfig.java
+++ src/main/java/pl/pb/tc/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
         ).formLogin(form -> form
                 .loginPage("/login")
-                .defaultSuccessUrl("/", true)
+                .defaultSuccessUrl("/dashboard", true)
                 .permitAll()
         ).logout(logout -> logout
                 .logoutSuccessUrl("/?wylogowano")
